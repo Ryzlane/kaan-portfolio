cc7218bbb804b29af76d525ae5ed0e36c7aeaffa
Ryzlane <ryzlane.arsac@hetic.net>
2018-07-16 23:52:10 +0200

    💄 Cursor component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import 'styles/style.scss'
+import './Home.scss'
 
 import Background from 'components/Background'
+import Cursor from 'components/Cursor'
 
-const Home = () =>
-  <Background backgroundState='initial' />
+ const Home = ()=>
+  <div className='global-container'>
+    <Background backgroundState='initial'/>
+    <Cursor />
+  </div>
 
-  export default Home
+export default Home
