fa236ab78119a551748d026209fc3fdb265a45c0
Ryzlane <ryzlane.arsac@hetic.net>
2018-08-11 18:14:36 +0200

    🚚 💄 Some packages, style and moves

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import Vivus from 'vivus'
 
-import { LogoText } from './LogoElements'
-
+import LogoText from 'assets/logo/logotext.svg'
+import LogoFrame from 'assets/logo/logoframe.svg'
 import './Logo.scss'
 
 class Logo extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      vivus: undefined
+    }
+
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState ({ vivus: new Vivus('logo__frame', {duration: 200, start: 'manual', file: LogoFrame}, null) })
+  }
+
+  handleMouseEnter() {
+    this.state.vivus.play()
+  }
+
+  handleMouseLeave() {
+    this.state.vivus.reset()
+    this.state.vivus.stop()
+  }
+  
   render() {
     return (
-      <div className='logo'>
-        <LogoText />
+      <div className='logo' onMouseEnter={() => this.handleMouseEnter()} onMouseLeave={() => this.handleMouseLeave()}>
+        <div id="logo__frame"></div>
+        <div className="logo__text">
+          <img src={LogoText} alt='logo' />
+        </div>
       </div>
     )
   }
